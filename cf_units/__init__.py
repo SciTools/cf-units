780c2a380e2cdee382e64d89d4eedc47de436c9b
Kristjan Onu <kristjan.onu@canada.ca>
2017-01-03 16:18:06 -0500

    Update UCAR UDUNITS URL
     - previous URL produces HTTP 404 error

--- cf_units/__init__.py
+++ cf_units/__init__.py
@@ -21,7 +21,7 @@ Provision of a wrapper class to support Unidata/UCAR UDUNITS-2, and the
 netcdftime calendar functionality.
 
 See also: `UDUNITS-2
-<http://www.unidata.ucar.edu/software/udunits/udunits-2/udunits2.html>`_.
+<http://www.unidata.ucar.edu/software/udunits>`_.
 
 """
 
