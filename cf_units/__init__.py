6a3eaae53a0f21d2f763ea8bc4ed1ab7208c6a7d
Martin Yeo <martin.yeo@metoffice.gov.uk>
2021-06-15 17:06:45 +0100

    Ignore __version__ for linting.

--- cf_units/__init__.py
+++ cf_units/__init__.py
@@ -40,7 +40,7 @@ from cf_units import _udunits2 as _ud
 from cf_units._udunits2 import (UT_ASCII, UT_ISO_8859_1, UT_LATIN1, UT_UTF8,
                                 UT_NAMES, UT_DEFINITION)
 
-from ._version import version as __version__
+from ._version import version as __version__  # noqa: F401
 
 __all__ = ['CALENDAR_STANDARD',
            'CALENDAR_GREGORIAN',
