ad6acf8e1a080618e420b3089faf18f2f21305a3
Filipe Fernandes <ocefpaf@gmail.com>
2021-07-28 20:43:26 -0300

    review actions

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 yum install -y udunits2-devel
-PYTHONS=("cp36-cp36m" "cp37-cp37m" "cp38-cp38" "cp39-cp39")
+PYTHONS=("cp37-cp37m" "cp38-cp38" "cp39-cp39")
+WHEELHOUSE="/github/workspace/wheelhouse/"
 
 cp /usr/include/udunits2/* /usr/include/
 
-SITECFG=cf_units/etc/site.cfg
-echo "[System]" > $SITECFG
-echo "udunits2_xml_path = /usr/share/udunits/udunits2.xml" >> $SITECFG
+export UDUNITS2_INCDIR="/usr/include/udunits2"
+export UDUNITS2_LIBDIR="/usr/lib64"
+export UDUNITS2_XML_PATH="/usr/share/udunits/udunits2.xml"
 
 for PYTHON in ${PYTHONS[@]}; do
     /opt/python/${PYTHON}/bin/pip install --upgrade pip wheel setuptools setuptools_scm build twine auditwheel
-    /opt/python/${PYTHON}/bin/python -m build --sdist --wheel . --outdir /github/workspace/wheelhouse/
+    /opt/python/${PYTHON}/bin/python -m build --sdist --wheel . --outdir ${WHEELHOUSE}
 done
 
-for whl in /github/workspace/wheelhouse/cf_units*.whl; do
+for whl in ${WHEELHOUSE} cf_units*.whl; do
     auditwheel repair $whl
 done
