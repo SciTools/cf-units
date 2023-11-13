8ca2c812abc130b66df25cee76a726bfe4813aef
Ruth Comer <10599679+rcomer@users.noreply.github.com>
2023-11-13 20:50:37 +0000

    ruff autofixes

--- cf_units/tests/test_coding_standards.py
+++ cf_units/tests/test_coding_standards.py
@@ -75,7 +75,7 @@ class TestLicenseHeaders:
         """
         # Check the ".git" folder exists at the repo dir.
         if not os.path.isdir(os.path.join(REPO_DIR, ".git")):
-            raise ValueError("{} is not a git repository.".format(REPO_DIR))
+            raise ValueError(f"{REPO_DIR} is not a git repository.")
 
         # Call "git whatchanged" to get the details of all the files and when
         # they were last changed.
